d9138dd33e65bea18927a1048c3b510de7aa0df7
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-03-17 19:47:17 +0600

    f of func ret

--- LeetJS__/2629/functionComposition.js
+++ LeetJS__/2629/functionComposition.js
@@ -1,3 +1,6 @@
+//https://www.freecodecamp.org/news/function-composition-in-javascript/
+
+
 function add(a, b) {
   return a + b;
 }
@@ -10,5 +13,18 @@ function square(val) {
   return val * val;
 }
 
-console.log(multi(add(5, 5)));
-console.log(square(add(5, 5)));
+// compose = (f1, f2) => value => f2( f1(value) );
+// console.log(multi(add(5, 5)));
+// console.log(square(add(5, 5)));
+function compositiion(f1, f2){
+  return function(a,b){
+    return f2(f1(a,b))
+  }
+}
+
+
+let results = compositiion(add, multi)
+
+console.log(results(2,3));
+
+
