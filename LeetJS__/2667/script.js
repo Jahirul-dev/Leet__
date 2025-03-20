320bda48739ef11063a5159e3afc1413435de0a2
Jahirul-dev <jahirul.jitu7@gmail.com>
2025-03-20 16:28:51 +0600

    solve

--- LeetJS__/2667/script.js
+++ LeetJS__/2667/script.js
@@ -1,7 +1,25 @@
-// immediate invoke
+// Normal function 
+ function Nfunc(a,b){
+    return a+b;
+}
+console.log(Nfunc(3,5));
 
+//Anonymous function
+let f = function(a,b){
+    return a+b;
+}
+console.log(f(5,7));
+
+
+// immediate invoke
 let func = function(a,b){
     return a+b;
 }(2,3)
 
 console.log(func);
+
+// Arrow function
+let fun = ((a,b)=>{
+    return a+b;
+}) 
+console.log(fun(2,5));
